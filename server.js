5c796096327c9c905b1015c6b320e231ab98a95d
Lucas Neves <nevessricardoo@gmail.com>
2024-10-30 16:00:21 -0300

    Connected the server with the database and made the server handle customer data for login and register

--- server.js
+++ server.js
@@ -19,6 +19,37 @@ db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => {
+    res.sendFile(_dirname + '/register.html');
+});
+// dar opção na página register que é a home para fazer o login caso já tenha conta, ou o inverso é melhor?
+// colocar os cookies
+
+app.get("/login", (req, res)=>{
+    res.sendFile(_dirname + "/login.html");
+});
+
+app.post("/dashboard", async (req, res)=>{
+    var userName = req.body.userName;
+    var password = req.body.password; 
+
+    const query = "SELECT * FROM public.users WHERE username = $1 AND password = $2"
+    const values = [userName, password]
+    try {
+
+        const { rows } = await db.query(query, values)
+
+        if (rows.length === 0) {
+            res.sendFile(__dirname + "/failLog.html")
+        } else {
+            res.send( `Hello ${userName}`)
+        }
+        
+    } catch (error) {
+        
+    }
+});
+
 app.post("/register", async (req, res) => {
 
     var firstName = req.body.firstName;
@@ -27,16 +58,34 @@ app.post("/register", async (req, res) => {
     var password = req.body.password;
 
 
-    const result = await db.query("INSERT INTO public.users (id, firstname, lastname, username, password, reg_date) VALUES ($1, $2, $3, $4, $5, $6)", [2, '2', '3', '4', '5', '2017-08-24 17:45:42']);
+    const query = "SELECT * FROM public.users WHERE username = $1 AND password = $2";
+    const values = [userName, password];
+    try {
 
-    console.log(result);
-    
+        const { rows } = await db.query(query, values);
+        
+        
+        if (rows.length > 0) {
 
-})
+            res.sendFile(_dirname + '/failReg.html');
+        } else {
+            // colocar hashing no password******************************
 
-app.get('/', (req, res) => {
-    res.sendFile(_dirname + '/register.html');
+            const result = await db.query("INSERT INTO public.users (firstname, lastname, username, password, reg_date) VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP)", [firstName, lastName, userName, password]);
+            
+        }
+            res.sendFile(_dirname + '/login.html');
+            
+    } catch (error) {
+
+        console.log(error);
+
+    }
+
+
+
 });
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}.`);
